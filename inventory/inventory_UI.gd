cd24fa5af58a5d91d89ee4c96ac3649ff3ba02f2
a02324041 <a02324041@usu.edu>
2025-02-28 22:16:45 -0700

    REALLY fixed merges

--- inventory/inventory_UI.gd
+++ inventory/inventory_UI.gd
@@ -1,6 +1,5 @@
 extends Control
 
-<<<<<<< HEAD
 @onready var inventory:Inventory = preload("res://inventory/player1_inventory.tres")
 @onready var slots:Array = $NinePatchRect/GridContainer.get_children()
 var is_open = false 
@@ -17,18 +16,6 @@ func update_slots():
 #_process function is how you grab input
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("open_inventory"):
-=======
-#to check if inventory is open or closed
-var is_open = false
-
-#Ready function is how you do stuff on game startup (I think)
-func _ready():
-	close_inventory()
-	
-#_process function is how you grab input
-func _process(delta: float) -> void:
-	if Input.get_action_strength("open_inventory"):
->>>>>>> 0955688
 		if is_open:
 			close_inventory()
 		else:
