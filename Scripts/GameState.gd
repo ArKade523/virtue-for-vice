5fa478894125b15b01c6807b6b917922d819a730
a02324041 <a02324041@usu.edu>
2025-03-01 05:20:03 -0700

    Fixing weird file structure

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -1,5 +1,19 @@
 extends Node
 
+#general player stats
+const max_health = 100
+const base_damage = 10
+
+#player teal stats
+var teal_health = max_health
+var teal_damage = base_damage
+var teal_attributes = ["handsome","dashing","considerate","good-personality","short-nose-hairs", "trimmed-toenails"]
+
+#blue player stats
+var blue_health = max_health
+var blue_damage = base_damage
+var blue_attributes = ["handsome","dashing","considerate","good-personality","short-nose-hairs", "trimmed-toenails"]
+
 var next_dungeon_map = {
 	"dungeon1": "res://Scenes/dungeon2.tscn",
 	"dungeon2": "res://Scenes/dungeon1.tscn",
