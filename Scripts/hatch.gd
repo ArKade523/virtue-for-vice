832bf22ce3788d38a32b2165f7dbc373ceeb8eef
Kade Angell <kade@angell.fyi>
2025-03-01 04:43:15 -0700

    Updated dungeons and added hatch fadeout anim

--- Scripts/hatch.gd
+++ Scripts/hatch.gd
@@ -1,22 +1,34 @@
 extends Area2D
 
 @onready var sprite: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $CanvasLayer/AnimationPlayer
+@onready var black_overlay: ColorRect = $CanvasLayer/BlackOverlay
 
 func _ready():
 	body_entered.connect(_on_body_entered)
 	
+func _process(delta: float) -> void:
+	if _all_enemies_defeated():
+		change_tile(sprite.texture.atlas, Rect2(144, 48, 16, 16))
+	
 func _on_body_entered(body):
 	if body is CharacterBody2D and _all_enemies_defeated():
-		change_tile(sprite.texture.atlas, Rect2(144, 48, 16, 16))
-		var next_scene = GameState.get_next_level(get_tree().current_scene.name)
-		if next_scene:
-			get_tree().change_scene_to_packed(next_scene)
+		_fade_to_black(body)
+		
 
 func _all_enemies_defeated() -> bool:
 	# Check if there are any enemies left in the current scene
 	var enemies = get_tree().get_nodes_in_group("enemies")
 	return enemies.size() == 0  # Return true if no enemies exist
 
+func _fade_to_black(body):
+	black_overlay.position = body.global_position - black_overlay.size / 2
+	animation_player.play("fadeOut")
+
+	await animation_player.animation_finished
+	var next_scene = GameState.get_next_level(get_tree().current_scene.name)
+	if next_scene:
+		get_tree().change_scene_to_packed(next_scene)
 
 func change_tile(tileset_texture: Texture2D, new_region: Rect2):
 	var atlas_texture = AtlasTexture.new()
