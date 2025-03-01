422f9183a991e7e325556288b6c4a9b384df2089
ReeceNielson <reecewnielson@gmail.com>
2025-03-01 03:54:47 -0700

    Added collisions and animations to goblin

--- Scripts/goblin.gd
+++ Scripts/goblin.gd
@@ -1,9 +1,14 @@
 extends CharacterBody2D
 
 const MOVEMENT_SPEED = 150
+const DAMAGE = 5
+var health = 20
+
 
 @onready var timer = $Timer
 @onready var view = $ShapeCast2D
+@onready var animation = $AnimatedSprite2D
+@onready var cooldown = $cooldown
 
 
 var x_mov = 0
@@ -12,6 +17,16 @@ var y_mov = 0
 func _process(delta):
 	velocity.x = x_mov
 	velocity.y = y_mov
+	if abs(velocity.x) > abs(velocity.y):
+		if velocity.x > 0:
+			animation.play("right_walk")
+		else:
+			animation.play("left_walk")
+	else:
+		if velocity.y > 0:
+			animation.play("down_walk")
+		else:
+			animation.play("up_walk")
 	var result: Array = view.collision_result
 	if not result.is_empty():
 		update_direction(Vector2(result[0].point.x - position.x ,result[0].point.y - position.y))	
@@ -28,3 +43,9 @@ func update_direction(direction):
 	y_mov = angle.y * MOVEMENT_SPEED
 	view.target_position.x = angle.x * 150
 	view.target_position.y = angle.y * 150
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("BIG OOF")
+	print(body.name)
+	#body.health -= DAMAGE
