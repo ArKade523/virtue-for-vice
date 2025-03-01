e0914e590e8ef61ade6144a05ecfd089aac764d9
ReeceNielson <reecewnielson@gmail.com>
2025-02-28 23:18:43 -0700

    Added skeleton mob with tracking

--- Scripts/skelly.gd
+++ Scripts/skelly.gd
@@ -15,24 +15,21 @@ func _process(delta):
 	velocity.y = y_mov
 	var result: Array = locator.collision_result
 	if not result.is_empty():
-		update_direction(Vector2(result[0].point.x - position.x ,result[0].point.y - position.y))
-		print("collided with: " + str(result[0].point.y))
-		print("collided with: " + str(result[0].point.x))		
+		update_direction(Vector2(result[0].point.x - position.x, result[0].point.y - position.y))	
 	move_and_slide()
 
-
-func _on_timer_timeout():
-	if not range.is_colliding():	
-		update_direction(Vector2(randi_range(-100, 100), randi_range(-100, 100)))
-
-func update_direction(direction):
+func update_direction(direction: Vector2):
 	var angle = direction.normalized()
 	if range.is_colliding():
 		var result: Array = locator.collision_result
 		if not result.is_empty():
-			x_mov = angle.x * MOVEMENT_SPEED
-			y_mov = angle.y * MOVEMENT_SPEED
-	x_mov = angle.x * MOVEMENT_SPEED
-	y_mov = angle.y * MOVEMENT_SPEED
-	view.target_position.x = angle.x * 150
-	view.target_position.y = angle.y * 150
+			x_mov = angle.x * MOVEMENT_SPEED * -1
+			y_mov = angle.y * MOVEMENT_SPEED * -1
+	elif direction.length() < 70:
+		x_mov = 0
+		y_mov = 0
+	else:
+		x_mov = angle.x * MOVEMENT_SPEED
+		y_mov = angle.y * MOVEMENT_SPEED
+	range.target_position.x = angle.x * 50
+	range.target_position.y = angle.y * 50
