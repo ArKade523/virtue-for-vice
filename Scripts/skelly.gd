5a49db96f3f3e795b45a32c28c9f316aee45d0ce
ReeceNielson <reecewnielson@gmail.com>
2025-02-28 22:44:42 -0700

    Makin a new nenenenememememmy

--- Scripts/skelly.gd
+++ Scripts/skelly.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
+
 const MOVEMENT_SPEED = 150
 
 @onready var timer = $Timer
-@onready var view = $ShapeCast2D
-
+@onready var locator = $Locator
+@onready var range = $range
 
 var x_mov = 0
 var y_mov = 0
@@ -12,7 +13,7 @@ var y_mov = 0
 func _process(delta):
 	velocity.x = x_mov
 	velocity.y = y_mov
-	var result: Array = view.collision_result
+	var result: Array = locator.collision_result
 	if not result.is_empty():
 		update_direction(Vector2(result[0].point.x - position.x ,result[0].point.y - position.y))
 		print("collided with: " + str(result[0].point.y))
@@ -21,11 +22,16 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-	if not view.is_colliding():	
+	if not range.is_colliding():	
 		update_direction(Vector2(randi_range(-100, 100), randi_range(-100, 100)))
 
 func update_direction(direction):
 	var angle = direction.normalized()
+	if range.is_colliding():
+		var result: Array = locator.collision_result
+		if not result.is_empty():
+			x_mov = angle.x * MOVEMENT_SPEED
+			y_mov = angle.y * MOVEMENT_SPEED
 	x_mov = angle.x * MOVEMENT_SPEED
 	y_mov = angle.y * MOVEMENT_SPEED
 	view.target_position.x = angle.x * 150
